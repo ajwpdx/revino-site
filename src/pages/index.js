bc6dd072f8946964bd733eda67abc2a57a0c753e
Alex Williams <alexjwilliams1@gmail.com>
2021-04-21 18:11:15 -0700

    added content

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,59 @@ const IndexPage = () => {
   return (
     <>
       <Header />
-      <section id="home">
-
+      {/* <section id="home">
         <img className="logo" src={logo} alt="revino logo" />
         <h2>WEBSITE COMING SOON</h2>
+      </section> */}
+      <section id="welcome-to-revino">
+        <div className='section-container'>
+          <h2>Welcome to Revino!</h2>
+          <p>We are building the refillable economy and ecosystem for glass bottles in the wine industry. Our purpose is to help develop a circular economy focused on the reuse and refilling of our glass bottles in Oregon wine communities. Why use a bottle once when it could be refilled 20 times</p>
+        </div>
       </section>
       <section id="how-it-works">
+        <div className='section-container'>
+          <h2>How It Works</h2>
+          <ul>
+            <li>You buy Revino bottles and fill them with your world renowned Oregon wine!</li>
+            <li>Consumers enjoy your wine.</li>
+            <li>Consumers can use Oregon's statewide recycling service, OBRC, to drop off their refillable containers.</li>
+            <li>Waste management picks up our fully recyclable crates from participating collection locations.</li>
+            <li>We work some magic with our proprietary washing system to inspect and wash your bottles to be cleaner than new.</li>
+          </ul>
+        </div>
+      </section>
+      <section id="why-reuse">
+        <div className='section-container'>
+          <h2>Why Reuse Glass Bottles?</h2>
+          <ul>
+            <li>3.37 billion single use bottles are directed to landfills each year.</li>
+            <li>Recycled glass is made up of more than 40% raw materials</li>
+            <li>The U.S produces over 4.8 billion bottles each year</li>
+          </ul>
+        </div>
+      </section>
+      <section id='sustainability'>
+        <div className="section-container">
+          <h2>Sustainability</h2>
+          <ul>
+            <li>Lower carbon footprint 95%</li>
+            <li>Less water 82%</li>
+            <li>Less energy 92%</li>
+          </ul>
+
+        </div>
+      </section>
+      <section id='contact'>
+        <div className="section-container">
+
+        <h2>The refillable bottle economy is here to stay, how are you going to play a role? </h2>
+        <p>Partner with us for the refillable movement</p>
+        <button>Sign Up</button>
+        </div>
 
       </section>
+
     </>
   )
 }
