b15f133524df5d93f8e2bdfab1b8ef15277a6d02
Alex Williams <alexjwilliams1@gmail.com>
2021-09-23 11:41:18 -0700

    fixed modal button

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,19 @@ module.exports = {
     title: "revino-site",
   },
   plugins: ["gatsby-plugin-sass",
-  {
-    resolve: "gatsby-plugin-manifest", 
-    options:{
-      name: "revino-site",
-      short_name: "revino",
-      start_url: "/",
-      background_color: '#1F2332',
-      theme_color: "#1F2332",
-      display: "minimal-ui",
-      icon: "src/images/revino-logo-blue.png"
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "revino-site",
+        short_name: "revino",
+        start_url: "/",
+        background_color: '#1F2332',
+        theme_color: "#1F2332",
+        display: "minimal-ui",
+        icon: "src/images/revino-logo-blue.png"
+      }
     }
-  }
   ],
 
 };
