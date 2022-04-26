dc6e081c31cd71b21834d5a07119210909228409
Alex Williams <alexjwilliams1@gmail.com>
2022-04-25 21:35:37 -0700

    fixed titleImage bug"

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -109,9 +109,7 @@ export const pageQuery = graphql`
           date(formatString: "MMMM DD, YYYY")
           title
           description
-          titleImage 
-           
-
+        
         }
       }
     }
