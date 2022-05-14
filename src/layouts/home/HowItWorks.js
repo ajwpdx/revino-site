07a7cdf6657fd0049a551425a413094bb2d8133e
Alex Williams <alexjwilliams1@gmail.com>
2022-05-13 20:38:01 -0700

    changed how it works wording

--- src/layouts/home/HowItWorks.js
+++ src/layouts/home/HowItWorks.js
@@ -15,9 +15,9 @@ const HowItWorks = (props) => {
           <ul>
             <li className='how-step'><img src={linedUpBottles} alt='four animated bottles'/><p>You buy Revino bottles and fill them with your world renowned Oregon wine! Consumers enjoy your wine.</p> </li>
             <li className='how-arrow' alt='arrow'><img src={arrow} alt="arrow"/></li>
-            <li className='how-step narrow-img'><img src={recyclingBin} alt='four animated bottles'/><p>Consumers can use Oregon's statewide recycling service, OBRC, to drop off their refillable containers.</p></li>
+            <li className='how-step narrow-img'><img src={recyclingBin} alt='four animated bottles'/><p>Consumers can drop off their refillable bottles.</p></li>
             <li className='how-arrow' alt='arrow'><img src={arrow} alt="arrow"/></li>
-            <li className='how-step'><img src={bottlesInCar} alt='four animated bottles'/><p> Waste management picks up our fully recyclable crates from participating collection locations.</p></li>
+            <li className='how-step'><img src={bottlesInCar} alt='four animated bottles'/><p>We collect the refillable bottles from participating locations.</p></li>
             <li className='how-arrow' alt='arrow'><img src={arrow} alt="arrow"/></li>
             <li className='how-step narrow-img'><img src={bottleFloating} alt='four animated bottles'/><p> We work some magic with our proprietary washing system to inspect and wash your bottles to be cleaner than new.</p></li>
           </ul>
